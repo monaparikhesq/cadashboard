a424822e6a61f286a905eac305ba8ba7ef0c87ca
Mona Parikh <mona.parikh.esq@gmail.com>
2012-07-25 10:14:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cadashboard::Application.config.secret_token = 'c8ff37ac660ef57052f061a0169b0f4989fed182e1c96555a4251dbeb4e6257e6c364343ba080e4458cd9451d507dca829dfd30e1193bba0ac50d657f501e773'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Cadashboard::Application.routes.draw do
+  root to: 'pages#home'
+end
