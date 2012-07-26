736db9926e5d3870749d746612f80533d5832135
Mona Parikh <mona.parikh.esq@gmail.com>
2012-07-26 11:34:58 -0400

    updated session#create notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to current_user, notice: "Welcome to Code Academy Dashboard!"
+    redirect_to current_user, notice: "Welcome to Code Academy Dashboard! Please fill in your blog & github link by clicking the 'Settings' tab."
   end
   
   def destroy
